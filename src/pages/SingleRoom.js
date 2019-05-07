06f44a2aed7f6a04868a362a1c5c4d176c19d281
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-07 12:55:05 +0100

    adding content

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        Hello from single room
+      </div>
+    )
+  }
+}
+export default  SingleRoom;
