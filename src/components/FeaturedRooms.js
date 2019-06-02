2d388764bda3d4f56afe807ba04732bfbcc2884d
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-02 14:58:42 +0100

    adding room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import {RoomContext} from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 class FeaturedRooms extends Component {
     static contextType = RoomContext;
+
     render() {
+        let {loading, featuredRooms: rooms} = this.context;
+        //console.log(rooms);
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+
         return (
-            <div>
-                hello from featured rooms 
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms"/>
+                <div className="featured-rooms-center">
+                    {loading ? <Loading/> : rooms}
+                </div>
+            </section>
         )
     }
 }
--- src/context.js
+++ src/context.js
@@ -8,7 +8,30 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: false
+        loading: true
+    }
+
+    //get the data 
+    componentDidMount(){
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
+        //console.log(rooms);
+    }
+
+    formatData(items){
+       let tempItems = items.map(item => {
+           let id = item.sys.id;
+           let images = item.fields.images.map(image => image.fields.file.url);
+           let room = {...item.fields, images, id}
+           return room;
+       });
+       return tempItems;
     }
 
     render() {
